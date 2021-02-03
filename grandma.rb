c8ba8d5f8c2c6e4b8bdf65d3b5207593a2c8dac7
Michael Dobak <mdobak44@icloud.com>
2021-02-03 23:35:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ require 'pry'
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase 
-    return "NO, NOT SINCE 1938"
+    return "NO, NOT SINCE 1938!"
   elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   else phrase == "I LOVE YOU GRANDMA!"
